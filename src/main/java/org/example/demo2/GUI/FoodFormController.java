3bf843d6689bf3bb91b5a09000f8db973b4c88ef
das41k <armenpetrosan319@gmail.com>
2024-11-08 14:08:17 +0500

    шоколадки не работают

--- src/main/java/org/example/demo2/GUI/FoodFormController.java
+++ src/main/java/org/example/demo2/GUI/FoodFormController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
+import org.example.demo2.Models.Chocolate;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -39,5 +41,38 @@ public class FoodFormController implements Initializable {
                 FOOD_CHOCOLATE,
                 FOOD_COOKIE
         ));
+
+        cmbFoodType.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            updatePanes((String) newValue);
+        });
+
+        cmbChocolateType.setConverter(new StringConverter <Chocolate.Type>() {
+            @Override
+            public String toString(Chocolate.Type object) {
+                switch (object) {
+                    case WHITE:
+                        return "Белый";
+                    case BLACK:
+                        return "Черный";
+                    case MILK:
+                        return "Молочный";
+                }
+                return null;
+            }
+
+            @Override
+            public Chocolate.Type fromString(String string) {
+                return null;
+            }
+        });
+        updatePanes("");
+    }
+    public void updatePanes(String newValue) {
+        this.fruitPane.setVisible(newValue.equals(FOOD_FRUIT));
+        this.fruitPane.setManaged(newValue.equals(FOOD_FRUIT));
+        this.chocolatePane.setVisible(newValue.equals(FOOD_CHOCOLATE));
+        this.chocolatePane.setManaged(newValue.equals(FOOD_CHOCOLATE));
+        this.cookiePane.setVisible(newValue.equals(FOOD_COOKIE));
+        this.cookiePane.setManaged(newValue.equals(FOOD_COOKIE));
     }
 }
